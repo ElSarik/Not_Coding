9b2a77708fbc7c065ea37235c80328309a35e5f0
Elias Sarikakis <sarikakisilias@gmail.com>
2022-01-12 13:55:03 +0200

    Small structure fix

--- challenges.py
+++ challenges.py
@@ -1,6 +1,9 @@
 def main():
+
     TotalValueOfCharacters()
+
     return 0
+
 
 def TotalValueOfCharacters():
 
@@ -26,54 +29,54 @@ def TotalValueOfCharacters():
     return 0
 
 
-    # Requesting user input
-    def request_input():
+# Requesting user input
+def request_input():
 
-        sequence = input('Please write a sequence of alphabetic characters: ')
+    sequence = input('Please write a sequence of alphabetic characters: ')
 
-        if sequence.isalpha():
-            return sequence
-        else:
-            print('Your input must contain only alphabetic characters!\n')
-
-
-    # Adding characters a - z to a list
-    def characters_init():
-
-        characters = []
-
-        for char in range (97, 122+1):
-            characters.append(chr(char))
-
-        return characters
+    if sequence.isalpha():
+        return sequence
+    else:
+        print('Your input must contain only alphabetic characters!\n')
 
 
-    # Converting uppercase characters to lowercase
-    def upper_to_lowercase(user_input):
+# Adding characters a - z to a list
+def characters_init():
 
-        user_input_list = list(user_input)
+    characters = []
 
-        # Converting every uppercase character to lowercase
-        for index, char in enumerate(user_input_list):
-            if ord(char) < 97:
-                user_input_list[index] = chr(ord(char) + 32)
+    for char in range (97, 122+1):
+        characters.append(chr(char))
 
-        return user_input_list
+    return characters
 
 
-    # Calculation of character value
-    def string_value_calculation(user_input):
+# Converting uppercase characters to lowercase
+def upper_to_lowercase(user_input):
 
-        characters = characters_init()
+    user_input_list = list(user_input)
 
-        sum = 0
-        
-        # Adding the sum of all the characters based on their index value + 1
-        for char in user_input:
-            value = characters.index(char)
-            sum += value + 1
+    # Converting every uppercase character to lowercase
+    for index, char in enumerate(user_input_list):
+        if ord(char) < 97:
+            user_input_list[index] = chr(ord(char) + 32)
 
-        return(sum)
+    return user_input_list
+
+
+# Calculation of character value
+def string_value_calculation(user_input):
+
+    characters = characters_init()
+
+    sum = 0
+    
+    # Adding the sum of all the characters based on their index value + 1
+    for char in user_input:
+        value = characters.index(char)
+        sum += value + 1
+
+    return(sum)
 
 
 if __name__ == "__main__":
