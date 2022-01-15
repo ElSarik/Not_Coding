e635064f9154ddfcbd9e11033f0cb4810a6be880
Elias Sarikakis <sarikakisilias@gmail.com>
2022-01-15 13:59:11 +0200

    Added challenge 2

--- challenges.py
+++ challenges.py
@@ -1,8 +1,18 @@
 def main():
 
-    TotalValueOfCharacters()
+    # TotalValueOfCharacters()
+    
+    a = [1, 2, 3]
+    print(f"Original list: {a}")
+    print(f"Reversed list: {ReverseList_Slicing(a)}")
 
     return 0
+
+
+def ReverseList_Slicing(list):
+    reversed_list = list[::-1]
+
+    return reversed_list
 
 
 def TotalValueOfCharacters():
