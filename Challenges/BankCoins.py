dd4da90cd2bce786c59a5621a13498ba31024ea7
Elias Sarikakis <sarikakisilias@gmail.com>
2022-01-27 13:05:57 +0200

    Added coin calculation function
    Dismantling the GRK value in 500, 100, 25, 10, 5, 1 change

--- Challenges/BankCoins.py
+++ Challenges/BankCoins.py
@@ -1,5 +1,63 @@
 def main():
     return
 
+
+def BankCoins(GRK):
+    
+    total_change = 0
+    change = 0
+    
+
+    if GRK >= 500:
+        change = int(GRK / 500)
+        GRK -= change * 500
+
+        total_change += change
+        change = 0
+
+
+    if GRK >= 100:
+        change = int(GRK / 100)
+        GRK -= change * 100
+
+        total_change += change
+        change = 0
+
+
+    if GRK >= 25:
+        change = int(GRK / 25)
+        GRK -= change * 25
+
+        total_change += change
+        change = 0
+
+
+    if GRK >= 10:
+        change = int(GRK / 10)
+        GRK -= change * 10
+
+        total_change += change
+        change = 0
+
+
+    if GRK >= 5:
+        change = int(GRK / 5)
+        GRK -= change * 5
+
+        total_change += change
+        change = 0
+
+
+    if GRK >= 1:
+        change = int(GRK / 1)
+        GRK -= change * 1
+
+        total_change += change
+        change = 0
+
+
+    return total_change
+
+
 if __name__ == '__main__':
     main()
